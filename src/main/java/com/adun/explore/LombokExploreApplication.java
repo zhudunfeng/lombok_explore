9ddea819a99aa3aa27f60bdd5784af33518f0bbe
zhudunfeng <2101580047@qq.com>
2022-11-26 12:59:15 +0800

    first commit

---
+++ src/main/java/com/adun/explore/LombokExploreApplication.java
@@ -0,0 +1,13 @@
+package com.adun.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokExploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokExploreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adun/explore/LombokExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adun.explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokExploreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
